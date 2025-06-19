a442c1b6434484b2a6c0f976435c6dfc69f2e4a5
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-20 05:08:05 +0530

    Create Solution.java

---
+++ Step1/Basics/While-loop/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void calculateMultiples(int n) {
+        // code here
+        int i = 10;
+        while(i>=1){
+            System.out.print(i*n+" ");
+            --i;
+        }
+    }
+}
