5c220079fd853c09bf292ce0b171e34c47e98821
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-20 05:07:18 +0530

    Create Solution.cpp

---
+++ Step1/Basics/While-loop/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void calculateMultiples(int n) {
+        // code here
+        int i = 10;
+        while(i>=1){
+            cout<<(i*n)<<" ";
+            --i;
+        }
+    }
+};
