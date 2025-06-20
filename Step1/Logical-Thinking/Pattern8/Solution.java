5ecea053a9f336ebae3b68d06e7c569e43ff263d
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-20 08:08:39 +0530

    Update Solution.java

--- Step1/Logical-Thinking/Pattern8/Solution.java
+++ Step1/Logical-Thinking/Pattern8/Solution.java
@@ -2,7 +2,7 @@ public class Main
 {
     static void star(int n){
         for(int i = n; i>0; --i){
-            for(int j = 0; j<=(n-i); ++j){
+            for(int j = 0; j<=(n-i-1); ++j){
                 System.out.print(" ");
             }
             for(int k = 0; k<(2*i-1); ++k){
