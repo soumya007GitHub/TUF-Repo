f6d306367e5c52de5d2aa1baa4df765540a679cc
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-20 08:12:05 +0530

    Create Solution.java

---
+++ Step1/Logical-Thinking/Pattern9/Solution.java
@@ -0,0 +1,29 @@
+public class Main
+{
+    static void star(int n){
+    for(int i = 0; i<n; ++i){
+            for(int j = 0; j<(n-i-1); ++j){
+                System.out.print(" ");
+            }
+            for(int k = 0; k<(2*i+1); ++k){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}
+static void reversedStar(int n){
+    for(int i = n; i>0; --i){
+            for(int j = 0; j<=(n-i-1); ++j){
+                System.out.print(" ");
+            }
+            for(int k = 0; k<(2*i-1); ++k){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+}
+	public static void main(String[] args) {
+		star(5);
+		reversedStar(5);
+	}
+}
