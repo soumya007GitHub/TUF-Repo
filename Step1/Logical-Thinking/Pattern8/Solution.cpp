9995a45ca13dda7c8aa0e24dbf32a7fe8e9e5635
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-20 08:08:27 +0530

    Update Solution.cpp

--- Step1/Logical-Thinking/Pattern8/Solution.cpp
+++ Step1/Logical-Thinking/Pattern8/Solution.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 void star(int n){
     for(int i = n; i>0; --i){
-            for(int j = 0; j<=(n-i); ++j){
+            for(int j = 0; j<=(n-i-1); ++j){
                 cout<<" ";
             }
             for(int k = 0; k<(2*i-1); ++k){
