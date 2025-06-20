cdc09cef942ef1ef52b260ee57e2bb1b2a5fccc5
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-20 08:04:43 +0530

    Create Solution.java

---
+++ Step1/Logical-Thinking/Pattern8/Solution.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    static void star(int n){
+        for(int i = n; i>0; --i){
+            for(int j = 0; j<=(n-i); ++j){
+                System.out.print(" ");
+            }
+            for(int k = 0; k<(2*i-1); ++k){
+                System.out.print("*");
+            }
+           System.out.println();
+        }
+    }
+	public static void main(String[] args) {
+		star(5);
+	}
+}
