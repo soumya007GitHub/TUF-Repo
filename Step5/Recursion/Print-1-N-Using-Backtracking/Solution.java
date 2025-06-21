dd3a23e5d89b614e9f88650c3b58f7f3380d6a49
Soumya Tripathy <130821564+soumya007GitHub@users.noreply.github.com>
2025-06-22 04:07:28 +0530

    Update Solution.java

--- Step5/Recursion/Print-1-N-Using-Backtracking/Solution.java
+++ Step5/Recursion/Print-1-N-Using-Backtracking/Solution.java
@@ -1,12 +1,12 @@
 public class Main
 {
     // USING BACKTRACKING TECHNIQUE
-    static void run(int counter, int num){
+    static void run(int counter){
         if(counter < 1){return;}
-        run(counter - 1, num);
+        run(counter - 1);
         System.out.println(counter);
     }
 	public static void main(String[] args) {
-		run(4, 4);
+		run(4);
 	}
 }
